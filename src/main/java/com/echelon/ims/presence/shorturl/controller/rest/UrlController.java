5411ca70c78719565522b1782e43b3287c421068
Khalid Zoabi <kzoabi@outlook.com>
2016-10-09 06:56:55 -0400

    Adding ERROR, WARN, and INFO logging to the UrlController class

--- src/main/java/com/echelon/ims/presence/shorturl/controller/rest/UrlController.java
+++ src/main/java/com/echelon/ims/presence/shorturl/controller/rest/UrlController.java
@@ -49,6 +49,7 @@ public class UrlController
     {
        if (hash == null || hash.trim().isEmpty())
        {
+           logger.error("Input parameter is invalid. {invalid.hash}.");
            response.sendError(HttpServletResponse.SC_BAD_REQUEST);  // HTTP 400
        }
               
@@ -56,10 +57,12 @@ public class UrlController
        
        if (url == null)
        {
+           logger.error("Hash not found in database. {notfound.hash} [hash={}].", hash);
            response.sendError(HttpServletResponse.SC_NOT_FOUND);  // HTTP 404
        }
        else
        {
+           logger.info("Hash found in database. Redirecting [hash={}] to [longUrl={}].", hash, url.getLongUrl());
            response.sendRedirect(url.getLongUrl());
        }
     }
@@ -69,10 +72,12 @@ public class UrlController
     {
         if (!urlValidator.isValid(longUrl))
         {   
+            logger.error("Input parameter is not a valid url. {invalid.longUrl} [longUrl={}].", longUrl);
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);  // HTTP 400
         }
-            
+        
         String hash = Hashing.murmur3_32().hashString(longUrl, StandardCharsets.UTF_8).toString();
+        logger.info("Hashed the url. [hash={}] [longUrl={}].", hash, longUrl);
         
         int counter = 0;  // hash collision retry counter
         while (urlRepository.findOneByHash(hash) != null && counter < 2)
@@ -87,22 +92,27 @@ public class UrlController
              *  again twice.
              */
             
-            logger.info("Hash collision detected! Regenerating the hash...");    
+            logger.warn("Hash collision detected. {collision.hash} Regenerating the hash.");    
             hash = Hashing.murmur3_32().hashString(System.currentTimeMillis() + longUrl, StandardCharsets.UTF_8).toString();
+            logger.info("Hashed the url. [hash={}] [longUrl={}].", hash, longUrl);
             counter++;
         }
         
         // If the code above is unable to generate a unique hash, return a server error
         if (counter == 2)
         {
+            logger.error("Too many hash collisions detected. {collision.hash} Tried to regenerate hash {} times for [longUrl={}].", counter, longUrl);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);  // HTTP 500
         }
         
         // TODO make sure hashed value is a clean value (readable I vs i, also maybe all lowercase? also no bad words)
+        String shortUrl = baseUrl + hash;
         
-        urlRepository.save(new Url(hash, longUrl));
+        urlRepository.save(new Url(hash, shortUrl, longUrl));
         
-        return new ResponseEntity<String>(baseUrl + hash, HttpStatus.CREATED);  // HTTP 201
+        logger.info("Hash saved to database. [hash={}] [shortUrl={}] [longUrl={}].", hash, shortUrl, longUrl);
+        
+        return new ResponseEntity<String>(shortUrl, HttpStatus.CREATED);  // HTTP 201
     }
 
 }
