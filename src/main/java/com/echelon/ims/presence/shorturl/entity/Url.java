b87f77d9f4fd148c568b9764db21c18b20ddcd88
Khalid Zoabi <kzoabi@outlook.com>
2016-10-09 06:56:01 -0400

    Adding shortUrl to Url database entity to persist the generated shortUrl
    Persisting the shortUrl that was generated is good for tracking, in the scenario where at a future date the base URL may change.

--- src/main/java/com/echelon/ims/presence/shorturl/entity/Url.java
+++ src/main/java/com/echelon/ims/presence/shorturl/entity/Url.java
@@ -12,20 +12,22 @@ public class Url
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String hash;
+    private String shortUrl;
     private String longUrl;
 
     protected Url() {}
 
-    public Url(String hash, String longUrl)
+    public Url(String hash, String shortUrl, String longUrl)
     {
         this.hash = hash;
+        this.shortUrl = shortUrl;
         this.longUrl = longUrl;
     }
 
     @Override
     public String toString()
     {
-        return String.format("Url[id=%d, hash='%s', longUrl='%s']", id, hash, longUrl);
+        return String.format("Url[id=%d, hash='%s', shortUrl='%s', longUrl='%s']", id, hash, shortUrl, longUrl);
     }
     
     public String getLongUrl()
